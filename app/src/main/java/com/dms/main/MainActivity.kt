35c5e0cf0f7e723f139134da7f540f739e1f765f
Sashi Manandhar <sashimdr8@gmail.com>
2018-10-26 10:41:47 +0545

    update

--- app/src/main/java/com/dms/main/MainActivity.kt
+++ app/src/main/java/com/dms/main/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.dms.main
 
-import android.databinding.DataBindingUtil
 import android.os.Bundle
-import com.dms.databinding.ActivityMainBinding
 import com.dms.R
 
 /**
@@ -10,14 +8,9 @@ import com.dms.R
  */
 class MainActivity : BaseActivity() {
 
-
-    private lateinit var binding: ActivityMainBinding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,
-                R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
     }
 
